5cc1462ea80dac3666f36ad39bd5bfef5be01327
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2021-10-20 08:58:52 -0400

    feat: allow to pass custom sdk for fullstory
    fixes #30

--- src/util.ts
+++ src/util.ts
@@ -1,4 +1,12 @@
 import { EventHint } from '@sentry/types';
+import * as Sentry from '@sentry/browser';
+
+/**
+ * Returns true if Fullstory is installed correctly.
+ */
+export function doesFullStoryExist() {
+  return !!window[window['_fs_namespace']];
+}
 
 /**
  * Get the project ID from a Sentry DSN
@@ -25,3 +33,28 @@ export const getOriginalExceptionProperties = (hint?: EventHint) => {
 
   return {};
 };
+
+/**
+ * Returns the sentry URL of the error. If we cannot get the URL, return a
+ * string saying we cannot.
+ */
+export function getSentryUrl(args: { hint?: EventHint, sentryOrg: string, baseSentryUrl: string }) {
+  try {
+    // No docs on this but the SDK team assures me it works unless you bind another Sentry client
+    const { dsn } = Sentry.getCurrentHub().getClient()?.getOptions() || {};
+    if (!dsn) {
+      console.error('No sn');
+      return 'Could not retrieve url';
+    }
+    if (!args.hint) {
+      console.error('No event hint');
+      return 'Could not retrieve url';
+    }
+    const projectId = getProjectIdFromSentryDsn(dsn);
+    return `${args.baseSentryUrl}/organizations/${args.sentryOrg}/issues/?project=${projectId}&query=${args.hint.event_id}`;
+  } catch (err) {
+    console.error('Error retrieving project ID from DSN', err);
+    //TODO: Could put link to a help here
+    return 'Could not retrieve url';
+  }
+}
