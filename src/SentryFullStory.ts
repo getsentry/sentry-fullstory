50a9056e1f9d9ccdc3948d2bea183d373656a14b
Greg Wogan-Browne <greg.wogan-browne@afterpaytouch.com>
2022-01-07 05:30:08 +1100

    Catch exceptions when accessing FullStory API methods (#58)
    Catch exceptions when accessing FS API methods, as otherwise the actual error that occurred gets swallowed by the FS exception.
    
    Also upgraded some dependencies (rollup being the main one).

--- src/SentryFullStory.ts
+++ src/SentryFullStory.ts
@@ -25,19 +25,19 @@ class SentryFullStory {
     this.baseSentryUrl = options.baseSentryUrl || 'https://sentry.io';
   }
 
-  private doesFullStoryExist() {
+  private static doesFullStoryExist() {
     return !!window[window['_fs_namespace']];
   }
 
   setupOnce() {
     Sentry.addGlobalEventProcessor((event: Event, hint?: EventHint) => {
-      //Returns the sentry URL of the error
-      //If we cannot get the URL, return a string saying we cannot
-      const getSentryUrl = () => {
+
+      const getSentryUrl = (): string => {
+        // Returns the sentry URL of the error
+        // If we cannot get the URL, return a string saying we cannot
         try {
-          //No docs on this but the SDK team assures me it works unless you bind another Sentry client
-          const { dsn } =
-            Sentry.getCurrentHub().getClient()?.getOptions() || {};
+          // No docs on this but the SDK team assures me it works unless you bind another Sentry client
+          const { dsn } = Sentry.getCurrentHub().getClient()?.getOptions() || {};
           if (!dsn) {
             console.error('No DSN');
             return 'Could not retrieve url';
@@ -55,25 +55,35 @@ class SentryFullStory {
         }
       };
 
-      const self = Sentry.getCurrentHub().getIntegration(SentryFullStory);
-      // Run the integration ONLY when it was installed on the current Hub AND isn't a transaction
-      if (self && event.type !== 'transaction' && this.doesFullStoryExist()) {
+      const getFullStoryUrl = (): string => {
         // getCurrentSessionURL isn't available until after the FullStory script is fully bootstrapped.
         // If an error occurs before getCurrentSessionURL is ready, make a note in Sentry and move on.
         // More on getCurrentSessionURL here: https://help.fullstory.com/develop-js/getcurrentsessionurl
+        try {
+          return FullStory.getCurrentSessionURL(true) || 'Current session URL API not ready'
+        } catch (e) {
+          const reason = e instanceof Error ? e.message : String(e)
+          return `Unable to get url: ${reason}`
+        }
+      }
+
+      const self = Sentry.getCurrentHub().getIntegration(SentryFullStory);
+      // Run the integration ONLY when it was installed on the current Hub AND isn't a transaction
+      if (self && event.type !== 'transaction' && SentryFullStory.doesFullStoryExist()) {
         event.contexts = {
           ...event.contexts,
           fullStory: {
-            fullStoryUrl:
-              FullStory.getCurrentSessionURL(true) ||
-              'current session URL API not ready',
+            fullStoryUrl: getFullStoryUrl()
           },
         };
-        // FS.event is immediately ready even if FullStory isn't fully bootstrapped
-        FullStory.event('Sentry Error', {
-          sentryUrl: getSentryUrl(),
-          ...util.getOriginalExceptionProperties(hint),
-        });
+        try {
+          FullStory.event('Sentry Error', {
+            sentryUrl: getSentryUrl(),
+            ...util.getOriginalExceptionProperties(hint),
+          });
+        } catch (e) {
+          console.debug('Unable to report sentry error details to FullStory')
+        }
       }
       return event;
     });
